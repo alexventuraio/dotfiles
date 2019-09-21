ae5f06b86f33976c3188481f78851259dfd6b660
Alejandro Ventura Contreras <soyalexventura@gmail.com>
2019-09-20 19:55:33 -0500

    Add two new colorschemes but only use "material" one and set the corresponding config for mvim and vim.

--- setup.sh
+++ setup.sh
@@ -95,6 +95,8 @@ curl -L https://bit.ly/janus-bootstrap | bash
 mkdir ~/.janus
 cd ~/.janus
 fancy_echo "Installing plugings for Janus for vim..."
+#git clone https://github.com/GertjanReynaert/cobalt2-vim-theme
+git clone https://github.com/kaicataldo/material.vim
 git clone https://github.com/wakatime/vim-wakatime
 git clone https://github.com/vim-scripts/UltiSnips
 git clone https://github.com/itchyny/lightline.vim
