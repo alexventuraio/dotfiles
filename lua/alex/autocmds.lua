4eb274f2b0ac1b0177f854cd5cccf02850cd6dac
Alex Ventura <me@alexventura.dev>
2022-11-12 22:45:35 -0600

    Add a new auto-command to refresh the current buffer after getting focus on it.

--- lua/alex/autocmds.lua
+++ lua/alex/autocmds.lua
@@ -7,3 +7,9 @@ cmd [[
     autocmd TextYankPost * silent! lua vim.highlight.on_yank()
   augroup end
 ]]
+
+-- Refresh current buffer on focus gain
+--  Reload Buffers on focus https://github.com/neovim/neovim/issues/20082#issuecomment-1288228566
+cmd [[
+  autocmd FocusGained * checktime
+]]
