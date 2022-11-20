e2732828086f409f7b3df2b66c6c161996c7247d
Alejandro Ventura <soyalexventura@gmail.com>
2022-11-20 13:35:20 -0600

    Add new function to configure LSP diagnostics and show them nicer.

--- lua/alex/lsp.lua
+++ lua/alex/lsp.lua
@@ -56,3 +56,31 @@ for _, lsp in ipairs(servers) do
     flags = lsp_flags,
   })
 end
+
+local function setup_diagnostics_config()
+  local signs = { Error = '', Warn = '', Hint = '', Info = '' }
+  for type, icon in pairs(signs) do
+    local hl = 'DiagnosticSign' .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+  end
+
+  local config = {
+    virtual_text = false, -- disable virtual text
+    signs = true,
+    update_in_insert = true,
+    underline = true,
+    severity_sort = true,
+    float = {
+      focusable = true,
+      style = 'minimal',
+      border = 'rounded',
+      source = 'always',
+      -- header = '',
+      -- prefix = '',
+    },
+  }
+
+  vim.diagnostic.config(config)
+end
+
+setup_diagnostics_config()
