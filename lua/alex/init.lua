332226ca88777e991ade29af389a61d0b6ec5c78
Alejandro Ventura <soyalexventura@gmail.com>
2022-11-19 22:07:13 -0600

    Add a sample config to be used later on to migrate to full Lua config as entry point instead of Vim script.

--- lua/alex/init.lua
+++ lua/alex/init.lua
@@ -14,3 +14,9 @@ require('alex.bufferline')
 require('alex.lualine')
 require('alex.miscellaneous')
 require('alex.neovide')
+
+-- Possibly user this script to move the content of the `init.vim` file here or just source it from here
+-- https://www.reddit.com/r/neovim/comments/mgc0pg/embedding_vimscript_functions_in_lua/
+-- vim.api.nvim_exec([[
+--   source /path/to/file.vim
+-- ]], false)
