26bd26c1eb19ab3793b6629066ececf5f10c7dc0
Alejandro Ventura <soyalexventura@gmail.com>
2022-11-19 21:54:14 -0600

    Add config for active and inactive sections to show the full path for files.

--- lua/alex/lualine.lua
+++ lua/alex/lualine.lua
@@ -11,4 +11,26 @@ lualine.setup({
     component_separators = { left = '', right = '' },
     section_separators = { left = '', right = '' },
   },
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        file_status = true,      -- Displays file status (readonly status, modified status)
+        newfile_status = true,   -- Display new file status (new file means no write after created)
+        full_path = true,        -- 1: Relative path
+        path = 1,
+      }
+    },
+  },
+  inactive_sections = {
+    lualine_c = {
+      {
+        'filename',
+        file_status = true,      -- Displays file status (readonly status, modified status)
+        newfile_status = true,   -- Display new file status (new file means no write after created)
+        full_path = true,        -- 1: Relative path
+        path = 1,
+      }
+    },
+  },
 })
