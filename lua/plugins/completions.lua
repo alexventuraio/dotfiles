80715d1f276504151bc7385bb77fb514732fbca7
Alex Ventura <me@alexventura.dev>
2024-01-23 01:51:18 -0600

    Configure completions and snippets with LSP.

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,11 +1,14 @@
 return {
   {
+    'hrsh7th/cmp-nvim-lsp'
+  },
+  {
     -- For luasnip users.
     'L3MON4D3/LuaSnip',
     dependencies = {
       'saadparwaiz1/cmp_luasnip',
       'rafamadriz/friendly-snippets',
-    }
+    },
   },
   {
     'hrsh7th/nvim-cmp',
@@ -36,15 +39,15 @@ return {
           ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         }),
         sources = cmp.config.sources({
-          -- { name = 'nvim_lsp' },
+          { name = 'nvim_lsp' },
           -- { name = 'vsnip' }, -- For vsnip users.
           { name = 'luasnip' }, -- For luasnip users.
           -- { name = 'ultisnips' }, -- For ultisnips users.
           -- { name = 'snippy' }, -- For snippy users.
         }, {
-            { name = 'buffer' },
-          })
+          { name = 'buffer' },
+        }),
       })
     end
-  }
+  },
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,11 +16,18 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
       -- Setup language servers.
       local lspconfig = require('lspconfig')
 
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities
+      })
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
