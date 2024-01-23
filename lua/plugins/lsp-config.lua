0b29a32c463e2f536e02f21942f77ff77c9d9a0e
Alex Ventura <me@alexventura.dev>
2024-01-23 12:43:17 -0600

    Configure LSP to install servers automatically.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -7,14 +7,14 @@ return {
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver" },
-      })
-    end
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
   },
   {
     "neovim/nvim-lspconfig",
+    lazy = false,
     config = function()
       -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
