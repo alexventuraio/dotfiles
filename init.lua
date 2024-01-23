3a7a5b24fcbceb7c5496b794352d700cf4a96c25
Alex Ventura <me@alexventura.dev>
2024-01-23 00:10:35 -0600

    Use a new module for the telescope plugin config.

--- init.lua
+++ init.lua
@@ -20,12 +20,6 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', 'ff', builtin.find_files, {})
-vim.keymap.set('n', 'fg', builtin.live_grep, {})
-vim.keymap.set('n', 'fb', builtin.buffers, {})
-vim.keymap.set('n', 'fh', builtin.help_tags, {})
-
 vim.keymap.set('n', '<leader>m', ':Neotree filesystem reveal left<CR>', {})
 
 local configs = require("nvim-treesitter.configs")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,10 +1,5 @@
 return {
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    -- or                              , branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {
     "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
   },
   {
