39a07bde92d631d098e7a9d822342d1f386afb6a
Alex Ventura <me@alexventura.dev>
2024-01-22 23:46:48 -0600

    Add telescope plugin for easier file finding.

--- init.lua
+++ init.lua
@@ -29,9 +29,20 @@ local plugins = {
     priority = 1000,
     opts = {},
   },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    -- or                              , branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
 }
 local opts = {}
 
 require("lazy").setup(plugins, opts)
 
 vim.cmd.colorscheme("tokyonight")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', 'ff', builtin.find_files, {})
+vim.keymap.set('n', 'fg', builtin.live_grep, {})
+vim.keymap.set('n', 'fb', builtin.buffers, {})
+vim.keymap.set('n', 'fh', builtin.help_tags, {})
