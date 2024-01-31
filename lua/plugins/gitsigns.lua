17b3ce537a23b9681cff9392a23a9a4e1a03f325
Alex Ventura <me@alexventura.dev>
2024-01-30 21:34:11 -0600

    Add new plugin for the git blame functionality.

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,9 +1,23 @@
 return {
-  'lewis6991/gitsigns.nvim',
-  config = function()
-    require('gitsigns').setup()
+  {
+    'tpope/vim-fugitive',
+    config = function()
+      vim.keymap.set('n', '<leader>gb', ':Git blame<CR>', {})
+      vim.keymap.set('n', '<leader>gs', ':Git<CR>', {})
+      vim.keymap.set('n', '<leader>gd', ':Git diff<CR>', {})
+      vim.keymap.set('n', '<leader>gl', ':Git log<CR>', {})
+      vim.keymap.set('n', '<leader>gc', ':Git commit<CR>', {})
+      vim.keymap.set('n', '<leader>gp', ':Git push<CR>', {})
+      vim.keymap.set('n', '<leader>gw', ':GBrowse<CR>', {})
+    end
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
 
-    vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', {})
-    vim.keymap.set('n', '<leader>gt', ':Gitsigns toggle_current_line_blame<CR>', {})
-  end
+      vim.keymap.set('n', '<leader>gh', ':Gitsigns preview_hunk<CR>', {})
+      vim.keymap.set('n', '<leader>gt', ':Gitsigns toggle_current_line_blame<CR>', {})
+    end
+  }
 }
