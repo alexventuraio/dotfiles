9c63860d22315e24d5497ab7b68ce4b9ad80fe9b
Alex Ventura <me@alexventura.dev>
2024-01-30 22:28:11 -0600

    Add some options for panel splitting and the cursor.

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,6 +3,13 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+
+vim.opt.splitbelow = true                          -- force all horizontal splits to go below current window
+vim.opt.splitright = true                          -- force all vertical splits to go to the right of current window
+vim.opt.cursorline = true                          -- highlight the current line
+vim.opt.cursorcolumn = true                        -- highlight cursor line underneath the cursor vertically.
+
+
 vim.g.mapleader = ' '
 
 -- Navigate vim panes better
