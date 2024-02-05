76eb98325fb61f0aeea4e5c677eb1add313317b7
Alex Ventura <me@alexventura.dev>
2024-02-05 14:35:04 -0600

    Add some navigation keymaps for normal mode.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,3 +4,22 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set('', '<Space>', '<Nop>', opts)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+
+
+
+-- NORMAL --
+
+-- Better window navigation
+-- vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
+-- vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
+-- vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
+-- vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+-- Clear highlights on hitting 'ESC' twice
+vim.keymap.set('n', '<esc><esc>', ':noh<return>', opts)
