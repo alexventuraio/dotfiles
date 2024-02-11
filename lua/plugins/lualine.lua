7a9a7a61a30558f8d260d7649b10f8b6dd49e287
Alex Ventura <me@alexventura.dev>
2024-02-10 23:33:06 -0600

    Switch to use the shortcut to pass options to the setup method from lazy.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,12 +1,8 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require('lualine').setup({
-      options = {
-        icons_enabled = true,
-        theme = 'auto',
-      }
-    })
-  end
+  config = {
+    icons_enabled = true,
+    theme = 'auto',
+  },
 }
