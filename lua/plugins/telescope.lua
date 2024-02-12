50236db16c312e437771075e4435d71ea2aa7df9
Alex Ventura <me@alexventura.dev>
2024-02-11 22:27:13 -0600

    Add lazy flag to the telescope plugin.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,8 @@
 return {
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    -- or                              , branch = '0.1.x',
+    -- or                          , branch = '0.1.x',
+    lazy = true,
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local builtin = require('telescope.builtin')
