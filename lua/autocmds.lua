d531b0cf19b6357ef98c45183b9440e94dbd1403
Alex Ventura <me@alexventura.dev>
2024-02-11 22:24:08 -0600

    Rewrite some auto-commands in Lua.

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,21 +1,27 @@
 local cmd = vim.cmd
 
 -- Highlight on yank
-cmd [[
-  augroup YankHighlight
-    autocmd!
-    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
-  augroup end
-]]
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('HighlightOnYank', {}),
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = 'IncSearch',
+      -- timeout = 145,
+    })
+  end,
+})
+
+-- Strip trailing whitespace on save
+vim.api.nvim_create_autocmd({'BufWritePre'}, {
+  group = vim.api.nvim_create_augroup('StripTrailingWhitespacesOnSave', {}),
+  pattern = '*',
+  command = [[%s/\s\+$//e]],
+})
 
 -- Auto-resize splits when Vim gets resized.
 cmd [[
   autocmd VimResized * wincmd =
-]]
-
--- Strip trailing whitespace on save
-cmd [[
-  autocmd BufWritePre * :%s/\s\+$//e
 ]]
 
 -- To close the Git Blame window from Fugitive with q
