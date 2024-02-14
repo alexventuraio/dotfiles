8012af4ae4450d1b60365c8426da8b1f3afbdd9b
Alex Ventura <me@alexventura.dev>
2024-02-13 23:04:45 -0600

    Clean up from debugging code.

--- lua/zoom.lua
+++ lua/zoom.lua
@@ -1,20 +1,3 @@
--- FROM: https://medium.com/@vinodkri/zooming-vim-window-splits-like-a-pro-d7a9317d40
--- very thanks for the tip, but maybe is better write a tiny vimscript snippet with something like this:
--- nnoremap Z :call ZoomWindowToggle()<cr>
--- let g:window_is_zoomed = 0
--- function! ZoomWindowToggle()
---  if g:window_is_zoomed
---    execute “normal \<c-w>=”
---    let g:window_is_zoomed = 0
---  else
---    execute “normal \<c-w>\|”
---    execute “normal \<c-w>_”
---    let g:window_is_zoomed = 1
---  endif
--- endfunction
--- It is better than remember 2 mappings for one single task.
-
-
 vim.g.window_is_zoomed = 0
 
 -- FROM: https://stackoverflow.com/a/69142336
@@ -25,22 +8,10 @@ end
 local function zoomWindowToggle()
   if vim.g.window_is_zoomed == 1 then
     print('IF - ' .. vim.g.window_is_zoomed)
-    -- execute "<C-w>="
-    -- vim.cmd.normal "<C-w>="
-    -- vim.cmd([[execute "normal <C-w>="]])
-    -- vim.cmd('execute \"normal <c-w>=\"')
     vim.cmd(t('normal <C-w>='))
     vim.g.window_is_zoomed = 0
   else
     print('ELSE - ' .. vim.g.window_is_zoomed)
-    -- execute "<C-w>|"
-    -- execute "<C-w>_"
-    -- vim.cmd.normal "<C-w>|"
-    -- vim.cmd.normal "<C-w>_"
-    -- vim.cmd([[execute "normal <C-w>|"]])
-    -- vim.cmd([[execute "normal <C-w>_"]])
-    -- vim.cmd('execute \"normal <c-w>|\"')
-    -- vim.cmd('execute \"normal <c-w>_\"')
     vim.cmd(t('normal <C-w>|'))
     vim.cmd(t('normal <C-w>_'))
     vim.g.window_is_zoomed = 1
