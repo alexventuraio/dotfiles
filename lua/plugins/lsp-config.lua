f3d9daab70435880f6a8610f4bed0ce86f9c2522
Alex Ventura <me@alexventura.dev>
2024-02-21 23:32:30 -0600

    Remove basic lsp config for now.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,23 +16,7 @@ return {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
-      -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-      -- Setup language servers.
-      local lspconfig = require('lspconfig')
-
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.tsserver.setup({
-        capabilities = capabilities
-      })
-
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+      -- FROM: https://github.com/hrsh7th/nvim-cmp/#setup
     end
   },
 }
