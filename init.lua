17867321c6d14649cd6e0a56c9470fb9357b5366
Alex Ventura <me@alexventura.dev>
2024-02-21 23:28:05 -0600

    Add some debug commands to remember.

--- init.lua
+++ init.lua
@@ -33,3 +33,9 @@ vim.cmd([[
   execute 'vnoremap <script> <special> <D-v>' paste#paste_cmd['v']
   execute 'inoremap <script> <special> <D-v>' paste#paste_cmd['i']
 ]])
+
+-- Debuggin options in Lua:
+-- :lua vim.print(vim.opt.whichwrap:get())  --> to see the output of the vim option in cmdline
+-- :verb set formatoptions --> to see the last file that modifies formatoptions setting
+-- :echo window_is_zoomed --> to see the value of a variable like vim.g.window_is_zoomed
+-- :set guifont? --> to see the value of a variable like vim.g.guifont
