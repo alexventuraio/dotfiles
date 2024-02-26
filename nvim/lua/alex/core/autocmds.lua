6acd65511689aa066c780f57cecafa058224ad18
Alex Ventura <me@alexventura.dev>
2024-02-26 17:26:40 -0600

    Add some useful description attribute for auto commands.

--- nvim/lua/alex/core/autocmds.lua
+++ nvim/lua/alex/core/autocmds.lua
@@ -1,7 +1,7 @@
 local cmd = vim.cmd
 
--- Highlight on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight on yank',
   group = vim.api.nvim_create_augroup('HighlightOnYank', {}),
   pattern = '*',
   callback = function()
@@ -12,8 +12,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- Strip trailing whitespace on save
 vim.api.nvim_create_autocmd({'BufWritePre'}, {
+  desc = 'Strip trailing whitespace on file save',
   group = vim.api.nvim_create_augroup('StripTrailingWhitespacesOnSave', {}),
   pattern = '*',
   command = [[%s/\s\+$//e]],
@@ -24,20 +24,24 @@ cmd [[
   autocmd VimResized * wincmd =
 ]]
 
--- Stops automatically commenting the next line when pressing Enter on a commented line.
--- Automatically insert the current comment leader after hitting <Enter> in Insert mode.
+-- Prevents from automatically inserting the current comment leader after hitting <Enter> in Insert mode.
 -- https://neovim.discourse.group/t/options-formatoptions-not-working-when-put-in-init-lua/3746/5
 -- https://superuser.com/a/271024
 vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
+  desc = 'Stops automatically commenting the next line when pressing Enter on a commented line',
   callback = function()
     cmd [[ set formatoptions-=cro ]]
   end,
 })
 
+-- Custom command for :GBrowser to work properly
 vim.api.nvim_create_user_command(
   'Browse',
   function (opts)
     vim.fn.system({ 'open', opts.fargs[1] })
   end,
-  { nargs = 1 }
+  {
+    nargs = 1,
+    desc = 'Open Github in the web browser pointing to the current file.',
+  }
 )
