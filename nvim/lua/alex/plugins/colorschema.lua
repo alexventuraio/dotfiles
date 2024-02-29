15f66f3f44a29bfdf302087ce98f9a3acc6fa088
Alex Ventura <me@alexventura.dev>
2024-02-28 18:41:01 -0600

    Switch to the tokyonight theme for now.

--- nvim/lua/alex/plugins/colorschema.lua
+++ nvim/lua/alex/plugins/colorschema.lua
@@ -5,7 +5,7 @@ return {
     -- lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme('nightfly')
+      -- vim.cmd.colorscheme('nightfly')
     end,
   },
   -- {
@@ -19,19 +19,19 @@ return {
   --     vim.keymap.set('n', '<leader>th', material_functions.find_style, {})
   --   end,
   -- },
-  -- {
-  --   'folke/tokyonight.nvim',
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     -- vim.cmd.colorscheme('tokyonight')
-  --     -- There are also colorschemes for the different styles.
-  --     -- vim.cmd.colorscheme('tokyonight-night')
-  --     -- vim.cmd.colorscheme('tokyonight-storm')
-  --     -- vim.cmd.colorscheme('tokyonight-day')
-  --     vim.cmd.colorscheme('tokyonight-moon')
-  --   end,
-  -- },
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme('tokyonight')
+      -- There are also colorschemes for the different styles.
+      -- vim.cmd.colorscheme('tokyonight-night')
+      -- vim.cmd.colorscheme('tokyonight-storm')
+      -- vim.cmd.colorscheme('tokyonight-day')
+      vim.cmd.colorscheme('tokyonight-moon')
+    end,
+  },
   -- {
   --   'EdenEast/nightfox.nvim',
   --   priority = 1000,
