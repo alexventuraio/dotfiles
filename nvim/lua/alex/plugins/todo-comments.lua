f3420abf26ef9f9359ac33f69e44cf756d6cd6d6
Alex Ventura <me@alexventura.dev>
2024-03-12 13:45:38 -0600

    Add some usage comments for the todo plugin and add a custom keyword.

--- nvim/lua/alex/plugins/todo-comments.lua
+++ nvim/lua/alex/plugins/todo-comments.lua
@@ -6,5 +6,20 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
+    keywords = {
+      FROM = { icon = ' ', color = 'from' },
+    },
+    colors = {
+      from = { '#ECF0F1' }
+    },
   }
 }
+
+-- FIX: alt = FIXME, BUG, FIXIT, ISSUE
+-- TODO:
+-- HACK:
+-- WARN: alt = WARNING, XXX
+-- PERF: alt = OPTIM, PERFORMANCE, OPTIMIZE
+-- NOTE: alt = INFO
+-- TEST: alt = TESTING, PASSED, FAILED
+-- FROM:
