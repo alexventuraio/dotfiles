462563f8cc6666f12b8549d2f5e3d977ed502530
Alex Ventura <me@alexventura.dev>
2024-04-05 13:40:40 -0600

    Delete vim config file and move the config to the vim-test lua config file.

--- nvim/after/plugin/vim-test.vim
+++
@@ -1,4 +0,0 @@
-" FROM: https://github.com/vim-test/vim-test?tab=readme-ov-file#strategies
-let test#strategy = 'neovim'
-" FROM: https://github.com/vim-test/vim-test?tab=readme-ov-file#vim8--neovim-terminal-position
-let test#neovim#term_position = 'horizontal botright 20'
--- nvim/lua/alex/plugins/vim-test.lua
+++ nvim/lua/alex/plugins/vim-test.lua
@@ -4,5 +4,10 @@ return {
   config = function()
     vim.keymap.set('n', '<leader>t', ':TestNearest<CR>')
     vim.keymap.set('n', '<leader>T', ':TestFile<CR>')
-  end
+
+    -- FROM: https://github.com/vim-test/vim-test?tab=readme-ov-file#strategies
+    vim.g['test#strategy'] = 'neovim'
+    -- FROM: https://github.com/vim-test/vim-test?tab=readme-ov-file#vim8--neovim-terminal-position
+    vim.g['test#neovim#term_position'] = 'horizontal botright 20'
+  end,
 }
