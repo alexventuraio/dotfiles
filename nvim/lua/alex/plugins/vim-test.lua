6cda58cff0d4972d2555d6a547d754c1014d05fb
Alex Ventura <me@alexventura.dev>
2024-04-05 13:46:02 -0600

    Use the keys section to lazy load the vim-test plugin on demand.

--- nvim/lua/alex/plugins/vim-test.lua
+++ nvim/lua/alex/plugins/vim-test.lua
@@ -1,13 +1,13 @@
 return {
   'vim-test/vim-test',
-  event = { 'BufReadPre', 'BufNewFile' },
   config = function()
-    vim.keymap.set('n', '<leader>t', ':TestNearest<CR>')
-    vim.keymap.set('n', '<leader>T', ':TestFile<CR>')
-
     -- FROM: https://github.com/vim-test/vim-test?tab=readme-ov-file#strategies
     vim.g['test#strategy'] = 'neovim'
     -- FROM: https://github.com/vim-test/vim-test?tab=readme-ov-file#vim8--neovim-terminal-position
     vim.g['test#neovim#term_position'] = 'horizontal botright 20'
   end,
+  keys = {
+    { '<leader>t', '<cmd>TestNearest<CR>', desc = 'In a test file runs the test nearest to the cursor, otherwise runs the last nearest test.' },
+    { '<leader>T', '<cmd>TestFile<CR>', desc = 'In a test file runs all tests in the current file, otherwise runs the last file tests.' },
+  },
 }
