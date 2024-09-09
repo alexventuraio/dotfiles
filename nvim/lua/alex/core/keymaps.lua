46e7364aff0d2ec09cb1827434afc3955499e8ce
Alex Ventura <me@alexventura.dev>
2024-09-09 12:23:08 -0600

    Remap the key combination to now delete the current opened buffer.

--- nvim/lua/alex/core/keymaps.lua
+++ nvim/lua/alex/core/keymaps.lua
@@ -19,7 +19,7 @@ vim.keymap.set('n', 'N', 'Nzzzv')
 vim.keymap.set('n', '<leader>sv', '<C-w>v') -- split window vertically
 vim.keymap.set('n', '<leader>sh', '<C-w>s') -- split window horizontally
 vim.keymap.set('n', '<leader>se', '<C-w>=') -- make split windows equal width & height
-vim.keymap.set('n', '<leader>sx', ':close<CR>') -- close current split window
+vim.keymap.set('n', '<leader>sx', ':bdelete<CR>') -- delete current opened buffer
 
 -- Increment/decrement
 vim.keymap.set('n', '+', '<C-a>')
