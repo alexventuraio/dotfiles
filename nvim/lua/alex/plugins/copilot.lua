aebe3b18c15097af08d76cfc721d7ff15060806c
Alex Ventura <me@alexventura.dev>
2025-03-15 22:06:07 -0600

    Add comment to Copilot key-mappings with the default ones.

--- nvim/lua/alex/plugins/copilot.lua
+++ nvim/lua/alex/plugins/copilot.lua
@@ -2,8 +2,8 @@ return {
   'github/copilot.vim',
   event = { 'BufReadPre', 'BufNewFile' },
   config = function()
-    -- vim.keymap.set('i', '<C-h>', '<Plug>(copilot-previous)')
-    -- vim.keymap.set('i', '<C-l>', '<Plug>(copilot-next)')
-    -- vim.keymap.set('i', '<C-j>', '<Plug>(copilot-suggest)')
+    -- vim.keymap.set('i', '<C-h>', '<Plug>(copilot-previous)') -- Alt - [
+    -- vim.keymap.set('i', '<C-l>', '<Plug>(copilot-next)')     -- Alt - ]
+    -- vim.keymap.set('i', '<C-j>', '<Plug>(copilot-suggest)')  -- Alt - \
   end,
 }
