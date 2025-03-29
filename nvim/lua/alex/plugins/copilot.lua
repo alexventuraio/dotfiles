6e56ab3e403a73f4f9bfbe016a5d4d1ed3038944
Alex Ventura <me@alexventura.dev>
2025-03-29 00:16:58 -0600

    Use another way to get the current default version of Nodejs on demand.

--- nvim/lua/alex/plugins/copilot.lua
+++ nvim/lua/alex/plugins/copilot.lua
@@ -2,7 +2,22 @@ return {
   'github/copilot.vim',
   event = { 'BufReadPre', 'BufNewFile' },
   config = function()
-    vim.g.copilot_node_command = '/Users/alex/.nvm/versions/node/v23.10.0/bin/node'
+    local handle = io.popen('nvm which default')
+
+    if not handle then -- Check if handle is nil
+      vim.notify('GitHub Copilot: Failed to find Node.js path', vim.log.levels.WARN)
+
+      return
+    end
+
+    local node_path = handle:read('*a'):gsub('\n', '')
+    handle:close()
+
+    if node_path and node_path ~= '' then
+      vim.g.copilot_node_command = node_path
+    else
+      vim.notify('GitHub Copilot: Node.js path not found', vim.log.levels.WARN)
+    end
 
     -- vim.keymap.set('i', '<C-h>', '<Plug>(copilot-previous)') -- Alt - [
     -- vim.keymap.set('i', '<C-l>', '<Plug>(copilot-next)')     -- Alt - ]
