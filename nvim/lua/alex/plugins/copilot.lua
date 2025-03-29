5b318e8f3819084dcc5210f42840ffcf9b034328
Alex Ventura <me@alexventura.dev>
2025-03-29 00:17:43 -0600

    Go back to the other way since it shows an error at startup.

--- nvim/lua/alex/plugins/copilot.lua
+++ nvim/lua/alex/plugins/copilot.lua
@@ -2,22 +2,7 @@ return {
   'github/copilot.vim',
   event = { 'BufReadPre', 'BufNewFile' },
   config = function()
-    local handle = io.popen('nvm which default')
-
-    if not handle then -- Check if handle is nil
-      vim.notify('GitHub Copilot: Failed to find Node.js path', vim.log.levels.WARN)
-
-      return
-    end
-
-    local node_path = handle:read('*a'):gsub('\n', '')
-    handle:close()
-
-    if node_path and node_path ~= '' then
-      vim.g.copilot_node_command = node_path
-    else
-      vim.notify('GitHub Copilot: Node.js path not found', vim.log.levels.WARN)
-    end
+    vim.g.copilot_node_command = '/Users/alex/.nvm/versions/node/v23.10.0/bin/node'
 
     -- vim.keymap.set('i', '<C-h>', '<Plug>(copilot-previous)') -- Alt - [
     -- vim.keymap.set('i', '<C-l>', '<Plug>(copilot-next)')     -- Alt - ]
