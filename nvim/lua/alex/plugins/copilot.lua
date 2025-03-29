7a6e10b671f3012d4578b09301f7673818473b34
Alex Ventura <me@alexventura.dev>
2025-03-29 00:14:35 -0600

    Add the path were to look for the Nodejs version.

--- nvim/lua/alex/plugins/copilot.lua
+++ nvim/lua/alex/plugins/copilot.lua
@@ -2,6 +2,8 @@ return {
   'github/copilot.vim',
   event = { 'BufReadPre', 'BufNewFile' },
   config = function()
+    vim.g.copilot_node_command = '/Users/alex/.nvm/versions/node/v23.10.0/bin/node'
+
     -- vim.keymap.set('i', '<C-h>', '<Plug>(copilot-previous)') -- Alt - [
     -- vim.keymap.set('i', '<C-l>', '<Plug>(copilot-next)')     -- Alt - ]
     -- vim.keymap.set('i', '<C-j>', '<Plug>(copilot-suggest)')  -- Alt - \
