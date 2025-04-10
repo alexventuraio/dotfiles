0c130894a8419e649c29bc5bb68454058fa6d522
Alex Ventura <me@alexventura.dev>
2025-04-10 11:36:13 -0600

    Add a brand new plugin to show and select color schemes on the fly.

---
+++ nvim/lua/alex/plugins/colorscheme-preview.lua
@@ -0,0 +1,13 @@
+return {
+  'joseche/colorscheme-preview',
+  opt = {
+    up_keys = { '<Up>', 'k' },
+    down_keys = { '<Down>', 'j' },
+    enter_keys = '<Enter>',
+    close_keys = { 'q', '<Esc>' },
+    dialog_width = 20,
+    dialog_height = 10,
+    verbose = true,  -- prints the current colorscheme
+  },
+  config = true,
+}
