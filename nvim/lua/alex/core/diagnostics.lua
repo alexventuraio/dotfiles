f128eab1f9bd6e09f998fcba009ed8cdafd26d7d
Alex Ventura <me@alexventura.dev>
2026-04-06 13:12:00 -0600

    Update deprecated keymaps and use the new Neovim way to config symbols for diagnostic errors.

--- nvim/lua/alex/core/diagnostics.lua
+++ nvim/lua/alex/core/diagnostics.lua
@@ -1,15 +1,8 @@
--- Change the Diagnostic symbols in the sign column (gutter)
--- (not in youtube nvim video)
-local signs = { Error = ' ', Warn = ' ', Hint = '󰠠 ', Info = ' ' }
-for type, icon in pairs(signs) do
-  local hl = 'DiagnosticSign' .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+-- FROM: https://dotfiles.substack.com/i/193173279/9-diagnostics-changes
+local severity = vim.diagnostic.severity
 
--- Diagnostic settings
 vim.diagnostic.config({
-  virtual_text = false,     -- disable virtual text
-  signs = true,
+  virtual_text = false, -- disable virtual text
   update_in_insert = true,
   underline = true,
   severity_sort = true,
@@ -17,9 +10,17 @@ vim.diagnostic.config({
     focusable = true,
     style = 'minimal',
     border = 'rounded',
-    source = 'always',
+    source = true,
     -- header = '',
     -- prefix = '',
+  },
+  signs = {
+    text = {
+      [severity.ERROR] = '',
+      [severity.WARN]  = '',
+      [severity.INFO]  = '',
+      [severity.HINT]  = '󰠠',
+    },
   },
 })
 
@@ -27,7 +28,7 @@ vim.diagnostic.config({
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 -- FROM: https://github.com/neovim/nvim-lspconfig?tab=readme-ov-file#suggested-configuration
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '[d', function() vim.diagnostic.jump({ count = -1, float = true }) end, { desc = 'diagnostic goto_prev' })
+vim.keymap.set('n', ']d', function() vim.diagnostic.jump({ count = 1, float = true }) end, { desc = 'diagnostic goto_next' })
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 vim.keymap.set('n', '<space>Q', vim.diagnostic.setqflist)
